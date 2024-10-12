1ed8ec465e251eb8d2960faa064e7ec80c1e95f1
root <root@friendly-wu.209-58-160-245.plesk.page>
2024-10-12 11:46:56 +0700

    change cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true, // deprecated in v4.25.8
+      headers: '*',
+      origin: ['*']
+    }
+  },
+  //'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
