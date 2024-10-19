d3bca435e59d04bcf8ec7294f67c7a525f5a4e21
MyNameFaro <info.naifaro@gmail.com>
2024-10-19 23:54:09 +0700

    First draft strapi 7 rvm api

--- src/api/rvm-api/routes/rvm-api.js
+++ src/api/rvm-api/routes/rvm-api.js
@@ -1,14 +1,69 @@
 module.exports = {
-    routes: [
-      {
-        method: 'GET',
-        path: '/customapi/hello',
-        handler: 'customapi.hello',
-        config: {
-          auth: false,  // Set this to true if you want authentication
-          policies: [],
-        },
+  routes: [
+    {
+      method: 'POST',
+      path: '/getProfile',
+      handler: 'rvm-api.getProfile',
+      config: {
+        auth: false,  // Set this to true if authentication is needed
+        policies: [],
       },
-    ],
-  };
+    },
+    {
+      method: 'POST',
+      path: '/calculatedBottlePoints',
+      handler: 'rvm-api.calculatedBottlePoints',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/calculatedCanPoints',
+      handler: 'rvm-api.calculatedCanPoints',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/accumulatePoints',
+      handler: 'rvm-api.accumulatePoints',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/donate',
+      handler: 'rvm-api.donate',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/activate',
+      handler: 'rvm-api.activate',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+    {
+      method: 'POST',
+      path: '/heartbeat',
+      handler: 'rvm-api.heartbeat',
+      config: {
+        auth: false,
+        policies: [],
+      },
+    },
+  ],
+};
+
   
